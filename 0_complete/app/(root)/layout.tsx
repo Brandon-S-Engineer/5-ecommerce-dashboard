f3382c24b1ab1b47fc0a5553981e17bbd55250fd
Brandon Soria <brandon.se.25@gmail.com>
2024-08-05 14:21:52 -0600

    u

--- 0_complete/app/(root)/layout.tsx
+++ 0_complete/app/(root)/layout.tsx
@@ -1,22 +1,18 @@
-import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs";
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
 
 // layout for the setup page (the page where the user creates their store) and show modal if the user don't have a store and if they do, redirect them to their store page (dashboard) instead of the setup page
 
 // flow will be from the sign in page to the setup page to the dashboard page
 // layout --> Root layout -----> check if the user exists( user is authenticated ) --> if not, redirect to sign in page ------> if user is authenticated then check if there is any store in the prisma db associated with the user if exists then redirect to the dashboard(of first store) ---> if no store is associated then navigate to create store modal
 
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/lib/prismadb';
 
-export default async function SetupLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function SetupLayout({ children }: { children: React.ReactNode }) {
   const { userId } = auth();
 
   if (!userId) {
-    redirect("/sign-in");
+    redirect('/sign-in');
   }
 
   const store = await prismadb.store.findFirst({
