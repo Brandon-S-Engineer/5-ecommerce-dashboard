fbc2ae1cc94840658c025956824377e4bc8ba679
Brandon <fortuneblue25@gmail.com>
2024-08-15 12:36:16 -0600

    'u'

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,56 +1,57 @@
-import { redirect } from 'next/navigation';
-import { auth } from '@clerk/nextjs'; // Clerk authentication
+// import { redirect } from 'next/navigation';
+// import { auth } from '@clerk/nextjs'; // Clerk authentication
 
-import Navbar from '@/components/navbar';
-import prismadb from '@/lib/prismadb'; // PrismaDB instance
+// import Navbar from '@/components/navbar';
+// import prismadb from '@/lib/prismadb'; // PrismaDB instance
 
-import { accounts, mails } from '@/lib/data'; // Account and mail data
-import { cookies } from 'next/headers';
+// import { accounts, mails } from '@/lib/data'; // Account and mail data
+// import { cookies } from 'next/headers';
 
-import { StickyDynamicNav } from '@/components/dynamic-sticky-nav';
+// import { StickyDynamicNav } from '@/components/dynamic-sticky-nav';
 
-export default async function DashboardLayout({ children, params }: { children: React.ReactNode; params: { storeId: string } }) {
-  const { userId } = auth(); // Check if we're logged in
-  const layout = cookies().get('react-resizable-panels:layout'); // Get layout cookie value
-  const collapsed = cookies().get('react-resizable-panels:collapsed'); // Get collapsed cookie value
+// export default async function DashboardLayout({ children, params }: { children: React.ReactNode; params: { storeId: string } }) {
+//   const { userId } = auth(); // Check if we're logged in
+//   const layout = cookies().get('react-resizable-panels:layout'); // Get layout cookie value
+//   const collapsed = cookies().get('react-resizable-panels:collapsed'); // Get collapsed cookie value
 
-  // Parse layout and collapsed data if they exist, otherwise default to undefined
-  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
-  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+//   // Parse layout and collapsed data if they exist, otherwise default to undefined
+//   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+//   const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
 
-  // Redirect to sign-in page if user is not authenticated
-  if (!userId) {
-    redirect('/sign-in');
-  }
+//   /* ------------------------------- Validation: ------------------------------ */
+//   // Redirect to sign-in page if user is not authenticated
+//   if (!userId) {
+//     redirect('/sign-in');
+//   }
 
-  // Fetch store from PrismaDB based on storeId and userId
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId,
-      userId,
-    },
-  });
+//   // Fetch store from PrismaDB based on storeId and userId
+//   const store = await prismadb.store.findFirst({
+//     where: {
+//       id: params.storeId,
+//       userId,
+//     },
+//   });
 
-  // Redirect to homepage if the store doesn't exist or doesn't belong to the user
-  if (!store) {
-    redirect('/');
-  }
+//   // Redirect to homepage if the store doesn't exist or doesn't belong to the user
+//   if (!store) {
+//     redirect('/');
+//   }
 
-  return (
-    <>
-      <div className='min-h-screen'>
-        <Navbar /> {/* Render Navbar */}
-        {children} {/* Render children elements */}
-      </div>
-      <div className='hidden flex-col md:flex'>
-        <StickyDynamicNav
-          accounts={accounts} // Pass accounts data to StickyDynamicNav
-          mails={mails} // Pass mails data to StickyDynamicNav
-          defaultLayout={defaultLayout} // Pass default layout
-          defaultCollapsed={defaultCollapsed} // Pass default collapsed state
-          navCollapsedSize={4} // Set navCollapsedSize to 4
-        />
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div className='min-h-screen'>
+//         <Navbar /> {/* Render Navbar */}
+//         {children} {/* Render children elements */}
+//       </div>
+//       <div className='hidden flex-col md:flex'>
+//         <StickyDynamicNav
+//           accounts={accounts} // Pass accounts data to StickyDynamicNav
+//           mails={mails} // Pass mails data to StickyDynamicNav
+//           defaultLayout={defaultLayout} // Pass default layout
+//           defaultCollapsed={defaultCollapsed} // Pass default collapsed state
+//           navCollapsedSize={4} // Set navCollapsedSize to 4
+//         />
+//       </div>
+//     </>
+//   );
+// }
