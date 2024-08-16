fc9d378e7b0c30713120dd941284360d545b2143
Brandon Soria <brandon.se.25@gmail.com>
2024-08-16 04:37:37 -0600

    u

--- 0_complete/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ 0_complete/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -23,48 +23,51 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   const graphRevenue = await getGraphRevenue(params.storeId);
 
   return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <Heading title="Dashboard" description="Overview of your store" />
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <Heading
+          title='Dashboard'
+          description='Overview of your store'
+        />
         <Separator />
 
-        <div className="grid gap-4 grid-cols-3">
+        <div className='grid gap-4 grid-cols-3'>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-6">
-              <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+            <CardHeader className='flex flex-row items-center justify-between space-y-6'>
+              <CardTitle className='text-sm font-medium'>Total Revenue</CardTitle>
 
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
+              <DollarSign className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{rupeeFormatter.format(totalRevenue)}</div>
+              <div className='text-2xl font-bold'>{rupeeFormatter.format(totalRevenue)}</div>
             </CardContent>
           </Card>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-6">
-              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+            <CardHeader className='flex flex-row items-center justify-between space-y-6'>
+              <CardTitle className='text-sm font-medium'>Sales</CardTitle>
 
-              <CreditCard className="h-4 w-4 text-muted-foreground" />
+              <CreditCard className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">+{salesCount}</div>
+              <div className='text-2xl font-bold'>+{salesCount}</div>
             </CardContent>
           </Card>
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-6">
-              <CardTitle className="text-sm font-medium">Products in Stock</CardTitle>
+            <CardHeader className='flex flex-row items-center justify-between space-y-6'>
+              <CardTitle className='text-sm font-medium'>Products in Stock</CardTitle>
 
-              <Package2 className="h-4 w-4 text-muted-foreground" />
+              <Package2 className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{stockCount}</div>
+              <div className='text-2xl font-bold'>{stockCount}</div>
             </CardContent>
           </Card>
         </div>
-        <Card className="col-span-4">
-          <CardHeader className="flex flex-row items-center justify-between space-y-6">
-            <CardTitle className="text-sm font-medium">Overview</CardTitle>
+        <Card className='col-span-4'>
+          <CardHeader className='flex flex-row items-center justify-between space-y-6'>
+            <CardTitle className='text-sm font-medium'>Overview</CardTitle>
           </CardHeader>
-          <CardContent className="pl-2">
+          <CardContent className='pl-2'>
             <Overview data={graphRevenue} />
           </CardContent>
         </Card>
