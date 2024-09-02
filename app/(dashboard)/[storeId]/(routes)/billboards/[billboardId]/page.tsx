35ecaf4c594616e7150edafbeb35d83cb092e5b3
Brandon Soria <brandon.se.25@gmail.com>
2024-09-01 19:07:00 -0600

    u

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,6 +1,21 @@
+import prismadb from '@/lib/prismadb';
+
 // billboardId corresponds to the dynamic segment [billboardId]
 const BillboardPage = async ({ params }: { params: { billboardId: string } }) => {
-  return <div>This is a form for billboards</div>;
+  // Fetch the billboard
+  const billboard = await prismadb.billboard.findFirst({
+    where: {
+      id: params.billboardId, // Match the billboard by its ID from the URL params
+    },
+  });
+
+  return (
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <BillboardForm />
+      </div>
+    </div>
+  );
 };
 
 export default BillboardPage;
