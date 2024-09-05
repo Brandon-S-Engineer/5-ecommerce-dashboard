f3c513caab9aaee6c3a25c90c48e86a549849a46
Brandon <fortuneblue25@gmail.com>
2024-09-04 19:06:03 -0600

    u

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { Trash } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
 
 interface ImageUploadProps {
   disabled?: boolean;
@@ -17,14 +20,36 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove,
   }, []);
 
   const onUpload = (result: any) => {
-    onChange(result.info.secure_url); //? On successful image upload, update the list of images with the new uploaded image URL.
+    //? On successful image upload, update the list of images with the new uploaded image URL.
+    onChange(result.info.secure_url);
   };
 
   if (!isMounted) {
     return null; // Prevent hydration errors
   }
 
-  return <div>ImageUpload</div>;
+  return (
+    <div>
+      <div className='mb-4 flex items-center gap-4'>
+        {/* Iterate over Images */}
+        {value.map((url) => (
+          <div
+            key={url}
+            className='relative w-[200px] h-[200px] rounded-md overfow-hidden'>
+            <div className='z-10 absolute top-2 right-2'>
+              <Button
+                type='button'
+                onClick={() => onRemove(url)}
+                variant='destructive'
+                size='icon'>
+                <Trash className='h-4 w-4' />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ImageUpload;
