854522daa37ca32960a91c0acb10d02b94576393
Brandon <fortuneblue25@gmail.com>
2024-09-30 16:59:50 -0600

    6 37 38

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -1,24 +1,20 @@
 import { format } from 'date-fns';
 
 import prismadb from '@/lib/prismadb';
-import { rupeeFormatter } from '@/lib/utils';
+import { formatter } from '@/lib/utils';
 
+import { ProductClient } from './components/client';
 import { ProductColumn } from './components/columns';
-import ProductsClient from './components/Client';
 
 const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
   const products = await prismadb.product.findMany({
-    where: {
-      storeId: params.storeId,
-    },
+    where: { storeId: params.storeId },
     include: {
       category: true,
       size: true,
       color: true,
     },
-    orderBy: {
-      createdAt: 'desc',
-    },
+    orderBy: { createdAt: 'desc' },
   });
 
   const formattedProducts: ProductColumn[] = products.map((item) => ({
@@ -26,17 +22,18 @@ const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
     name: item.name,
     isFeatured: item.isFeatured,
     isArchived: item.isArchived,
-    price: rupeeFormatter.format(item.price.toNumber()),
+    // price: formatter.format(item.price.toNumber()), // Is already a Number on Schema
+    price: formatter.format(item.price),
     category: item.category.name,
     size: item.size.name,
     color: item.color.value,
-    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+    createdAt: format(new Date(item.createdAt), 'MMMM do, yyyy'),
   }));
 
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
-        <ProductsClient data={formattedProducts} />
+        <ProductClient data={formattedProducts} />
       </div>
     </div>
   );
