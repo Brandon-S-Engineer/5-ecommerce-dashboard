ca3a4dda1c4bc0c861253c99b14d0a29de3059bb
Brandon Soria <brandon.se.25@gmail.com>
2024-10-27 23:41:35 -0600

    u

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -4,4 +4,44 @@ import { NextResponse } from 'next/server';
 import { stripe } from '@/lib/stipe';
 import prismadb from '@/lib/prismadb';
 
-const corsHeaders = {};
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(req: Request, { params }: { params: { storeId: string } }) {
+  const { productIds } = await req.json();
+
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse('Product IDs are required', { status: 400 });
+  }
+
+  const products = await prismadb.product.findMany({
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  products.forEach((product) => {
+    line_items.push({
+      quantity: 1,
+      price_data: {
+        currency: 'USD',
+        product_data: {
+          name: product.name,
+        },
+
+        unit_amount: product.price.toNumber() * 100,
+      },
+    });
+  });
+}
